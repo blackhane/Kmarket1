075595fa1d1499f6f8aee81b41dd0b0b1c0a73b9
qpdlxm145 <qpdlxm145@gmail.com>
2022-12-08 13:21:48 +0900

    2022/12/08 jsp변환 index 퍼블리싱완료

---
+++ Kmarket/src/main/java/kr/co/Kmarket/controller/utils/AdminSql.java
@@ -0,0 +1,38 @@
+package kr.co.Kmarket.controller.utils;
+
+public class AdminSql {
+	
+	public static final String INSERT_PRODUCT = "insert into `km_product` set "
+												+ "`cate1`=?,"
+												+ "`cate2`=?,"
+												+ "`prodName`=?,"
+												+ "`descript`=?,"
+												+ "`company`=?,"
+												+ "`price`=?,"
+												+ "`discount`=?,"
+												+ "`point`=?,"
+												+ "`stock`=?,"
+												+ "`delivery`=?,"
+												+ "`thumb1`=?,"
+												+ "`thumb2`=?,"
+												+ "`thumb3`=?,"
+												+ "`detail`=?,"
+												+ "`prodNo`=?,"
+												+ "`status`=?,"
+												+ "`duty`=?,"
+												+ "`receipt`=?,"
+												+ "`bizType`=?,"
+												+ "`brand`=?,"
+												+ "`origin`=?,"
+												+ "`material`=?,"
+												+ "`color`=?,"
+												+ "`size`=?,"
+												+ "`manufacturer`=?,"
+												+ "`country`=?,"
+												+ "`precautions`=?,"
+												+ "`date`=?,"
+												+ "`standard`=?,"
+												+ "`as`=?,"
+												+ "`delivery_date`=?";
+
+}
